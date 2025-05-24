7c2989f5313ea897b97300300ed55ec958fdf724
SanjarbekAI <sanjarbekwork@gmail.com>
2025-05-24 21:00:45 +0500

    db settings, .env created

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+from core.database_settings import execute_query
+from core.table_queries import initializing_tables
+
+
 def main():
-    pass
+    params = (12,)
+    query = "select * from users"
+    execute_query(query=query, params=params, fetch="all")
 
 
 if __name__ == '__main__':
+    initializing_tables()
     main()
